0bc61e4ee35a6d22a19f703cd120e5b1c1147309
Daniel <mr_schliesing@hotmail.com>
2023-05-11 15:59:08 -0800

    builder page

---
+++ src/components/Builder.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Container, Nav, Navbar, Stack, Image } from "react-bootstrap";
+import { Link, Outlet } from "react-router-dom";
+
+function Builder() {
+    return (
+        <div>
+          Test
+      </div>
+    )
+}
+
+export default Builder;
