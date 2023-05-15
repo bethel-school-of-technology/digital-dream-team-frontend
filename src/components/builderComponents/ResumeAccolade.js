2a5c895badd775f934d2c4e2ac05b7efdb1bd22b
Daniel <mr_schliesing@hotmail.com>
2023-05-14 18:46:50 -0800

    connected identity info, skills, job and proj to code. skills now add properly, and accomplishments are added based on submit plus. edu and certs are not hooked up but are close. nearly everything updates in a single stateful builderinfo obj

--- src/components/builderComponents/ResumeAccolade.js
+++ src/components/builderComponents/ResumeAccolade.js
@@ -1,10 +1,11 @@
 import React, {useState} from "react";
-import { Container, Nav, Navbar, Stack, Image, ListGroup, ListGroupItem, Card } from "react-bootstrap";
+import { Form,Container, Nav, Navbar, Stack, Image, ListGroup, ListGroupItem, Card, Col, Row } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 
 function ResumeAccolade(props) {
   let accolades = props.accolades
-
+  let bigA = props.which.a.charAt(0).toUpperCase()+ props.which.a.slice(1)
+  let bigB = props.which.b.charAt(0).toUpperCase()+ props.which.b.slice(1)
   function listAccolades(){
     return accolades.map(accolade =>{
       <ListGroupItem>
@@ -12,10 +13,20 @@ function ResumeAccolade(props) {
       </ListGroupItem>
     })
   }
+  function setAccolade(){
+    console.log("does stuff")
+  }
   return (
       <div>
         <ListGroup>
         {listAccolades()}
+        <ListGroupItem>
+        <Form onSubmit={(e) => setAccolade(e)}>
+          <Row><Col><Form.Control placeholder={bigA}  type="text" name={props.which.a}/></Col>
+          <Col><Form.Control placeholder={bigB}  type="text" name={props.which.b}/></Col>
+          <Col md={3}><Form.Control  placeholder="date acquired"  type="text" name="date"/></Col></Row>
+          </Form>
+        </ListGroupItem>
         </ListGroup>
     </div>
   )
--- src/components/builderComponents/ResumeItem.js
+++ src/components/builderComponents/ResumeItem.js
@@ -1,11 +1,11 @@
 import React, {useState} from "react";
-import {Form,Row, Container, Nav, Navbar, Stack, Image, ListGroup, ListGroupItem, Card, Col } from "react-bootstrap";
+import {Form,Row, Container, Nav, Navbar, Stack, Image, ListGroup, ListGroupItem, Card, Col, Button, InputGroup } from "react-bootstrap";
 import { Link, Outlet } from "react-router-dom";
 
 function ResumeItem(props) {
 
-  let item = props.item
-  let [bullets, setBullets] = useState(item.accomplishments)
+  let [bullets, setBullets] = useState(props.item.accomplishments)
+
 
   function Bullets(){
     if (!bullets) {
@@ -13,29 +13,69 @@ function ResumeItem(props) {
     }
     return bullets.map(bullet =>{
       return (
-        <ListGroupItem>
-          <Form><Form.Control></Form.Control></Form>
-        </ListGroupItem>
+        <Row><Col sm={{span:8, offset:4}}>
+            <Form onSubmit={(e) => removeAcc(e, props.which)}>
+              <InputGroup>
+                <Form.Control defaultValue={bullet} name="accomplishment"/>
+                <Button type="submit">-</Button>
+              </InputGroup>
+            </Form>
+        </Col></Row>
       )
     })
   }
+
+  function addAccomplishment(e, which){
+    props.updateAccomplishments(e, which)
+    if (bullets === undefined){
+      setBullets([e.target[0].value])
+    }else{
+      setBullets([...bullets, e.target[0].value])
+    }
+    e.preventDefault()
+    e.target.reset()
+  }
+
+  function removeAcc(e, which){
+    props.removeAccomplishment(e, which)
+    if (bullets.length() == 1){
+      setBullets([])
+    }else{
+      let index = bullets.indexOf(e.target[0].value)//should get text in form control
+      if (index !== -1){
+        let temp = bullets.splice(index,1)
+        setBullets(temp)
+      }
+    }
+    e.preventDefault()
+    e.target.reset()
+  }
+
   return (
       <div>
         <ListGroup>
-          <ListGroupItem>
-            <Form>
+          <ListGroupItem >
+            <Form onChange={(e)=>props.updateJobProj(e, props.which)}>
               <Row>
-                <Col><Form.Control placeholder="title"/></Col>
-                
-                <Col><Form.Control placeholder="Start"/></Col>
-                <Col><Form.Control placeholder="End"/></Col>
+                <Col md={5}><Form.Control placeholder="title" name="title"/></Col>
+                {props.which === "jobs" && <Col md={3}><Form.Control placeholder="Company" name="company"/></Col>}
+                <Col md={2}><Form.Control placeholder="Start" name="startdate"/></Col>
+                <Col md={2}><Form.Control placeholder="End" name="enddate"/></Col>
               </Row>
             </Form>
-          </ListGroupItem>
-            {Bullets()}
-            <ListGroupItem><Form><Form.Control placeholder="Accomplishments"/></Form>
-          </ListGroupItem>
+            </ListGroupItem>
         </ListGroup>
+        {Bullets()}
+
+        <Row><Col sm={{span:8, offset:4}}>
+            <Form onSubmit={(e) => addAccomplishment(e, props.which)}>
+              <InputGroup>
+                <Form.Control placeholder="Accomplishments" name="accomplishment"/>
+                <Button type="submit">+</Button>
+              </InputGroup>
+            </Form>
+        </Col></Row>
+
     </div>
   )
 }
