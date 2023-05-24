9eff09822781769f7b746cc1cec72beccf1a6949
Treasure Mwebe <titamwebe@gmail.com>
2023-05-24 20:32:52 +0300

    added home page discription

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,9 @@ const Home = () => {
 
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>About Resume Builder</h1>
+            <p>Resume Builder helps organize the skills section of your resume to better fit the job position you are applying for! We understand that when applying for various job positions, 
+                it is important to modify your skills on your resume, which can take time. With ChatGBT's OpenAi API, Resume Builder will do that for you!   </p>
         </div>
     );
 };
