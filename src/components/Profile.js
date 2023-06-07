5ea154ea3e1f951aac17d4a3afaa6423e984b46b
Daniel <mr_schliesing@hotmail.com>
2023-06-06 22:40:11 -0800

    addddeddd profile

---
+++ src/components/Profile.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../css/Home.css';
+
+const Profile = () => {
+
+    return (
+        <div>
+            <h1>Profile</h1>
+            <div >
+                <p>profile view</p>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
